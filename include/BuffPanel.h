1b312247dc686459308528a57bf94321f01e57d0
Osama Hassanein <player1os@gmail.com>
2017-04-09 14:17:23 +0200

    Prepare header for cross platform use

--- include/BuffPanel.h
+++ include/BuffPanel.h
@@ -1,9 +1,12 @@
-#pragma once
+#ifndef BUFFPANEL_H
+#define BUFFPANEL_H
 
-#ifdef BUFFPANEL_SDK_EXPORTS
+#ifdef _WIN32
+#ifdef BUFFPANEL_SDK_EXPORTS	
 #define BUFFPANEL_SDK_API __declspec(dllexport)
 #else
 #define BUFFPANEL_SDK_API __declspec(dllimport)
+#endif
 #endif
 
 #include <string>
@@ -18,3 +21,5 @@ namespace BuffPanel {
 		);
 	};
 }
+
+#endif
